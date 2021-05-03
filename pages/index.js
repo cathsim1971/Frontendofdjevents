b4f596b1af92b8fd61b0ca19926827aff74029c5
catherine <cathsim1971@icloud.com>
2021-05-03 19:33:54 +0100

    server side props

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
 import Layout from "@/components/Layout";
-import Head from "next/head";
+import { API_URL } from "@/config/index";
 
-export default function HomPage() {
+export default function HomPage({events}) {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: {events},
+    revalidate: 1
+}
 }
