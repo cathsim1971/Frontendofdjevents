3a73d4dd0a0eef3643f503c882f8c495cd880951
catherine <cathsim1971@icloud.com>
2021-05-03 18:35:47 +0100

    showcase

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
-import Layout from '../components/Layout'
-import Head from 'next/head'
+import Layout from "../components/Layout";
+import Head from "next/head";
 
 export default function HomPage() {
   return (
     <Layout>
-     
       <h1>Home</h1>
-      
     </Layout>
   );
 }
