13c3582008abb89c285a9de2daadd0669370b363
catherine <cathsim1971@icloud.com>
2021-05-05 11:12:28 +0100

    display events

--- components/Showcase.js
+++ components/Showcase.js
@@ -1,10 +1,10 @@
-import styles from "@/styles/Showcase.module.css";
+import styles from '@/styles/Showcase.module.css'
 
 export default function Showcase() {
   return (
-    <div className={styles.Showcase}>
-      <h1>Welcome To The Party</h1>
-      <h2>Find the hottest DJ Events </h2>
+    <div className={styles.showcase}>
+      <h1>Welcome To The Party!</h1>
+      <h2>Find the hottest DJ events</h2>
     </div>
-  );
+  )
 }
